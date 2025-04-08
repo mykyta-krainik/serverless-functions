d508d3d4278c29d9a4b891a891c25d10fd4167f6
Mykyta Krainik <nikinta2003@gmail.com>
2025-04-08 16:14:52 +0300

    add getting users' country

--- api/analytics/save.js
+++ api/analytics/save.js
@@ -1,8 +1,12 @@
+import crypto from 'crypto';
+
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const body = JSON.parse(req.body);
+    const id = crypto.randomUUID();
+    const country = req.headers['x-vercel-ip-country'] || 'unknown';
 
-    return res.status(200).json({ message: 'OK', event: body });
+    return res.status(200).json({ message: 'OK', event: body, id, country });
   }
   return res.status(405).json({ message: 'Method not allowed' });
 }
